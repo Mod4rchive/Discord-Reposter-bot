9fb3417ad75388299ebf7416bd5175fa79ac6088
reddiedev <105657266+reddiedev@users.noreply.github.com>
2023-10-31 22:48:44 +0800

    added config details

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,5 @@
-export const guildID = "993750582730952746";
-export const clientID = "993750582730952746";
+export const guildID = "1168457511117013002";
+export const clientID = "1168458180242702337";
 
-export const staffRoleID = "1016929939733356554";
+export const ownerID = "972731662666453012";
+export const developerID = "988205198323351562";
