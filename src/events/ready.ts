59c62ebaa202dabf7f5d2b2bb165a4fb43b4240c
reddiedev <105657266+reddiedev@users.noreply.github.com>
2023-10-31 22:49:18 +0800

    updated bot status

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -11,37 +11,38 @@ const event: BotEvent = {
         );
 
         // sets the bot activity
-        const setting = await client.prisma.setting.findUnique({
+        const setting = await client.prisma.setting.upsert({
             where: {
                 id: 1
-            }
+            },
+            create: {
+                id: 1
+            },
+            update: {}
         });
 
-        if (setting) {
-            let activity;
-            switch (setting.activityType) {
-                case "Playing":
-                    activity = ActivityType.Playing;
-                    break;
-                case "Streaming":
-                    activity = ActivityType.Streaming;
-                    break;
-                case "Listening":
-                    activity = ActivityType.Listening;
-                    break;
-                case "Watching":
-                    activity = ActivityType.Watching;
-                    break;
-                case "Competing":
-                    activity = ActivityType.Competing;
-                    break;
-            }
-
-            await client.user!.setActivity({
-                name: setting.activityName,
-                type: activity
-            });
+        let activity;
+        switch (setting.activityType) {
+            case "Playing":
+                activity = ActivityType.Playing;
+                break;
+            case "Streaming":
+                activity = ActivityType.Streaming;
+                break;
+            case "Listening":
+                activity = ActivityType.Listening;
+                break;
+            case "Watching":
+                activity = ActivityType.Watching;
+                break;
+            case "Competing":
+                activity = ActivityType.Competing;
+                break;
         }
+        await client.user!.setActivity({
+            name: setting.activityName,
+            type: activity
+        });
     }
 };
 
